3845da24a5bb275ff9b8e4d7d5bff035b4cbe965
Alexander Plakhotnik <plakhotnik2003@mail.ru>
2025-01-23 18:19:16 +0300

    finished block 2 (validation)

--- rest_lib/books/serializers.py
+++ rest_lib/books/serializers.py
@@ -5,3 +5,13 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['id', 'header', 'author', 'publication_year', 'genre']
+
+    def validate_header(self, value):
+        if not value:
+            raise serializers.ValidationError('Header is required.')
+        return value
+
+    def validate_author(self, value):
+        if not value:
+            raise serializers.ValidationError('Author is required.')
+        return value
